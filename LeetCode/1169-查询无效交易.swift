ead841b91b37b5615a52fdfdb1c3e95bf05667ff
李坤坤 <1403705809@qq.com>
2019-12-12 11:49:51 +0800

    1169-查询无效交易

---
+++ LeetCode/1169-查询无效交易.swift
@@ -0,0 +1,65 @@
+//
+//  1169-查询无效交易.swift
+//  LeetCode
+//
+//  Created by 李坤坤 on 2019/12/12.
+//  Copyright © 2019 李坤坤. All rights reserved.
+//
+
+import Foundation
+class Transaction {
+    let name:String
+    let time:Int
+    let price:Int
+    let city:String
+    var isVaild:Bool
+    
+    init(transaction: String) {
+        let info = transaction.split(separator: ",")
+        self.name = String(info[0])
+        self.time = Int(String(info[1]))!
+        self.price = Int(String(info[2]))!
+        self.city = String(info[3])
+        self.isVaild = self.price <= 1000
+    }
+    
+    func isVaild(with other:Transaction) -> Bool{
+        if self.isVaild {
+            self.isVaild = other.city == city ||
+                abs(other.time - time) > 60
+            other.isVaild = other.isVaild && self.isVaild
+        }
+        return self.isVaild
+    }
+    
+    func toString() -> String {
+        return "\(name),\(time),\(price),\(city)"
+    }
+}
+
+func invalidTransactions(_ transactions: [String]) -> [String] {
+    var map = [String:[Transaction]]()
+    for transaction in transactions {
+        let entiy = Transaction(transaction: transaction)
+        if var value = map[entiy.name]{
+            value.append(entiy)
+            map[entiy.name] = value
+        }else {
+            map[entiy.name] = [entiy]
+        }
+    }
+    var result = [Transaction]()
+    for (_,value) in map {
+        for transaction in value {
+            for other in value {
+                if !transaction.isVaild(with: other) {
+                    result.append(transaction)
+                    break
+                }
+            }
+        }
+    }
+    return result.map({ (transaction) in
+        return transaction.toString()
+    })
+}
