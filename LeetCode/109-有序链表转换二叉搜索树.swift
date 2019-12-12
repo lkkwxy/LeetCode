4fc8de92d8a0c593c7129f75940bbdbd59cc5da6
李坤坤 <1403705809@qq.com>
2019-12-12 16:03:59 +0800

    109-有序链表转换二叉搜索树

---
+++ LeetCode/109-有序链表转换二叉搜索树.swift
@@ -0,0 +1,29 @@
+//
+//  109-有序链表转换二叉搜索树.swift
+//  LeetCode
+//
+//  Created by 李坤坤 on 2019/12/12.
+//  Copyright © 2019 李坤坤. All rights reserved.
+//
+
+import Foundation
+//109-有序链表转换二叉搜索树
+class Solution {
+    func sortedListToBST(_ head: ListNode?) -> TreeNode? {
+        var sortArray = [Int]()
+        var head = head
+        while head != nil {
+            sortArray.append(head!.val)
+            head = head?.next
+        }
+        return sortArray.count == 0 ? nil : sortedArrayToBST(sortArray, start: 0, end: sortArray.count - 1)
+    }
+    func sortedArrayToBST(_ array: [Int], start: Int, end:Int) -> TreeNode? {
+        guard end > start else { return nil }
+        let mid = (start + end) / 2
+        let treeNode = TreeNode(array[mid])
+        treeNode.left = sortedArrayToBST(array, start: start, end: mid - 1)
+        treeNode.right = sortedArrayToBST(array, start: mid + 1, end: end)
+        return treeNode
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 * [621-任务调度器](https://leetcode-cn.com/problems/task-scheduler/)
 * [89-格雷编码](https://leetcode-cn.com/problems/gray-code/)
 * [1169-查询无效交易](https://leetcode-cn.com/problems/invalid-transactions/)
+* [109-有序链表转换二叉搜索树](https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/)
 
