b982bfb95bbd2dfbfdb4f54043f65e20e2ab5e8b
lkkwxy <1403705809@qq.com>
2020-12-19 15:44:43 +0800

    48. 旋转图像

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -42,3 +42,11 @@ func squareNumberCount(_ nums:[Int]) -> Int {
     }
     return count
 }
+var a = [
+    [ 5, 1, 9,11],
+    [ 2, 4, 8,10],
+    [13, 3, 6, 7],
+    [15,14,12,16]
+  ]
+rotate(&a)
+print(a)
